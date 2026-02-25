60650ec8179e94abb8b227e8f4a661dafac6e1d5
jasmine <jasmine2@unbc.ca>
2026-02-24 23:32:43 -0800

    works on phone

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -5,7 +5,6 @@ import { View, Text, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Ionicons } from '@expo/vector-icons';
 import { EmailProvider } from '../context/EmailContext';
 import { CartProvider } from '../context/CartContext';
