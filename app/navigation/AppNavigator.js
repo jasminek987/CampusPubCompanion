f963d09f79c974aa37ad23ba67ce706dec8044d2
jasmine <jasmine2@unbc.ca>
2026-02-25 20:53:23 -0800

    nothing

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -5,7 +5,6 @@ import { View, Text, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Ionicons } from '@expo/vector-icons';
 import { EmailProvider } from '../context/EmailContext';
 import { CartProvider } from '../context/CartContext';
@@ -70,31 +69,7 @@ function BottomTabNavigator() {
       <Tab.Screen name="Favorites" component={FavoritesScreen} />
       <Tab.Screen name="Info" component={InfoScreen} />
 
-      <Tab.Screen
-      name="ItemDetail"
-      component={ItemDetailScreen}
-      options={{ tabBarButton: () => null }}
-    />
-      <Tab.Screen
-        name="Cart"
-        component={CartScreen}
-        options={{ tabBarButton: () => null }}
-      />
-      <Tab.Screen
-      name="OrderSuccess"
-      component={OrderSuccessScreen}
-      options={{ tabBarButton: () => null }}
-     />
-      <Tab.Screen
-        name="Specials"
-        component={SpecialsScreen}
-        options={{ tabBarButton: () => null }}
-      />
-      <Tab.Screen
-        name="Checkout"
-        component={CheckoutScreen}
-        options={{ tabBarButton: () => null }}
-      />
+     
     </Tab.Navigator>
   );
 }
