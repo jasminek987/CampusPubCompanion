a8f640fac1e9209ba00f081a022189bc90e70455
jasmine <jasmine2@unbc.ca>
2026-03-17 16:02:55 -0700

    added backend files and did favourites api

---
+++ backend/routes/favoritesRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+// Temporary array to store favorites
+let favorites = [];
+
+router.get('/', (req, res) => {
+  res.json(favorites);
+});
+
+router.post('/', (req, res) => {
+  const newFavorite = req.body;
+  favorites.push(newFavorite);
+  res.json({ message: 'Favorite added!', favorite: newFavorite });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  favorites = favorites.filter(f => f.id !== id);
+  res.json({ message: 'Favorite removed!' });
+});
+
+module.exports = router;
